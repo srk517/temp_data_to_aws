941874acc4b609d9577a5088cc3db200eb053967
srk517 <fatehsali517@gmail.com>
2023-06-05 14:23:50 +0500

    ff

--- app_main.c
+++ app_main.c
@@ -1,7 +1,7 @@
 /* MQTT Mutual Authentication Example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
-
+working
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
